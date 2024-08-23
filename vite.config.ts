5535474737999df83b2e1fb42a763ce7a304b881
Su-Yong <simssy2205@gmail.com>
2024-08-23 19:36:45 +0900

    feat: setup config

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+
+import solid from 'vite-plugin-solid';
+import dts from 'vite-plugin-dts';
+
+const config = defineConfig({
+  root: './',
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'solid-flip',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['solid-js', 'solid-js/web'],
+      output: {
+        globals: {
+          'solid-js': 'solid',
+          'solid-js/web': 'solid',
+        },
+      },
+    },
+    minify: 'terser',
+  },
+  plugins: [
+    solid(),
+    dts(),
+  ],
+});
+
+export default config;
